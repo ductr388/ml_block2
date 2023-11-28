843b09aa936009f085f017b7214b4d4492950ae3
ductr388 <144364735+ductr388@users.noreply.github.com>
2023-11-28 22:22:59 +0100

    Updated assignment 2. Stopping criterion Works for 2 and 4 clusters

--- Duc.R
+++ Duc.R
@@ -148,6 +148,7 @@ sd(random_100)
 
 
 # Assignment 2                          ####
+# For 3 clusters                        ####
 set.seed(1234567890)
 max_it <- 500 # max number of EM iterations (increased from 100)
 min_change <- 0.1 # min change in log lik between two consecutive iterations
@@ -190,8 +191,8 @@ for(it in 1:max_it) {
   points(mu[2,], type="o", col="red")
   points(mu[3,], type="o", col="green")
   #points(mu[4,], type="o", col="yellow")
-  Sys.sleep(0.1) # Decreased from 0.5
-  # E-step: Computation of the  weights
+  Sys.sleep(0.01) # Decreased from 0.5
+  # E-step: Computation of the weights
   # Calculates the weights for each observation
   for(iter in 1:1000){
     x_d <- x[iter, ]
@@ -210,11 +211,16 @@ for(it in 1:max_it) {
     w[iter, ] <- c(prob_1, prob_2, prob_3) 
   }
   #Log likelihood computation.
-  llik[it] <- sum(log(w))
+  # Added 1e-16 to prevent log(0).
+  llik[it] <- sum(log(w+1e-16))
   cat("iteration: ", it, "log likelihood: ", llik[it], "\n")
   flush.console()
   # Stop if the lok likelihood has not changed significantly
-  # Your code here
+  if (it > 1){
+    if(abs(llik[it] - llik[it-1]) < abs(min_change)){
+      break
+    }
+  }
   # M-step: ML parameter estimation from the data and weights
   # Calculate new pi
   pi <- 1/1000 * colSums(w)
@@ -235,32 +241,171 @@ plot(llik[1:it], type="o")
 
 
 
-# Our own implementation algorithm      ####
-# Calculates the weights for each observation
-for(iter in 1:1000){
-  x_d <- x[iter, ]
-  
-  # Bernolli for (x_iter|mu_1), (x_iter|mu_2), (x_iter|mu_3)
-  bern_mu1 <- prod(mu[1,]^x_d * (1 - mu[1, ])^(1-x_d))
-  bern_mu2 <- prod(mu[2,]^x_d * (1 - mu[2, ])^(1-x_d))
-  bern_mu3 <- prod(mu[3,]^x_d * (1 - mu[3, ])^(1-x_d))
-  
-  # Probabilities that the observation belongs to each distribution
-  prob_1 <- pi[1] * bern_mu1 / sum(pi[1]*bern_mu1 + pi[2]*bern_mu2 + pi[3]*bern_mu3)
-  prob_2 <- pi[2] * bern_mu2 / sum(pi[1]*bern_mu1 + pi[2]*bern_mu2 + pi[3]*bern_mu3)
-  prob_3 <- pi[3] * bern_mu3 / sum(pi[1]*bern_mu1 + pi[2]*bern_mu2 + pi[3]*bern_mu3)
-  
-  # Probability that observation "iter" comes from each distribution
-  w[iter, ] <- c(prob_1, prob_2, prob_3) 
+
+# For 2 clusters                        ####
+true_pi <- vector(length = 3) # true mixing coefficients
+true_mu <- matrix(nrow=3, ncol=D) # true conditional distributions
+true_pi=c(1/3, 1/3, 1/3)
+true_mu[1,]=c(0.5,0.6,0.4,0.7,0.3,0.8,0.2,0.9,0.1,1)
+true_mu[2,]=c(0.5,0.4,0.6,0.3,0.7,0.2,0.8,0.1,0.9,0)
+true_mu[3,]=c(0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5)
+plot(true_mu[1,], type="o", col="blue", ylim=c(0,1))
+points(true_mu[2,], type="o", col="red")
+points(true_mu[3,], type="o", col="green")
+# Producing the training data
+for(i in 1:n) {
+  m <- sample(1:3,1,prob=true_pi)
+  for(d in 1:D) {
+    x[i,d] <- rbinom(1,1,true_mu[m,d])
+  }
 }
 
-# Calculate new pi
-pi <- 1/1000 * colSums(w)
+M=2 # number of clusters
+w <- matrix(nrow=n, ncol=M) # weights
+pi <- vector(length = M) # mixing coefficients
+mu <- matrix(nrow=M, ncol=D) # conditional distributions
+llik <- vector(length = max_it) # log likelihood of the EM iterations
 
-# Calculates new mu
-mu[1,] <- 1/sum(w[,1]) * colSums(w[,1] * x)
-mu[2,] <- 1/sum(w[,2]) * colSums(w[,2] * x)
-mu[3,] <- 1/sum(w[,3]) * colSums(w[,3] * x)
 
-# Calculates log-likelihood
-llik[1] <- sum(log(w))
+# Random initialization of the parameters
+pi <- runif(M,0.49,0.51)
+pi <- pi / sum(pi)
+for(m in 1:M) {
+  mu[m,] <- runif(D,0.49,0.51)
+}
+for(it in 1:max_it) {
+  plot(mu[1,], type="o", col="blue", ylim=c(0,1))
+  points(mu[2,], type="o", col="red")
+  #points(mu[3,], type="o", col="green")
+  #points(mu[4,], type="o", col="yellow")
+  Sys.sleep(0.5) # Decreased from 0.5
+  # E-step: Computation of the weights
+  # Calculates the weights for each observation
+  for(iter in 1:1000){
+    x_d <- x[iter, ]
+    
+    # Bernolli for (x1 given mu_1), (x1 given mu_2), (x1 given mu_3)
+    bern_mu1 <- prod(mu[1,]^x_d * (1 - mu[1, ])^(1-x_d))
+    bern_mu2 <- prod(mu[2,]^x_d * (1 - mu[2, ])^(1-x_d))
+    #bern_mu3 <- prod(mu[3,]^x_d * (1 - mu[3, ])^(1-x_d))
+    
+    # Probabilities that the observation belongs to each distribution
+    prob_1 <- pi[1] * bern_mu1 / sum(pi[1]*bern_mu1 + pi[2]*bern_mu2 )
+    prob_2 <- pi[2] * bern_mu2 / sum(pi[1]*bern_mu1 + pi[2]*bern_mu2 )
+    # prob_3 <- pi[3] * bern_mu3 / sum(pi[1]*bern_mu1 + pi[2]*bern_mu2 + pi[3]*bern_mu3)
+    
+    # Probability that observation 1 comes from each distribution
+    w[iter, ] <- c(prob_1, prob_2) 
+  }
+  #Log likelihood computation.
+  # Added 1e-16 to prevent log(0).
+  llik[it] <- sum(log(w+1e-16))
+  cat("iteration: ", it, "log likelihood: ", llik[it], "\n")
+  flush.console()
+  # Stop if the lok likelihood has not changed significantly
+  if (it > 1){
+    if(abs(llik[it] - llik[it-1]) < abs(min_change)){
+      break
+    }
+  }
+  # M-step: ML parameter estimation from the data and weights
+  # Calculate new pi
+  pi <- 1/1000 * colSums(w)
+  # Calculates new mu
+  mu[1,] <- 1/sum(w[,1]) * colSums(w[,1] * x)
+  mu[2,] <- 1/sum(w[,2]) * colSums(w[,2] * x)
+  # mu[3,] <- 1/sum(w[,3]) * colSums(w[,3] * x)
+}
+pi
+mu
+plot(llik[1:it], type="o")
+
+# For 4 clusters                        ####
+# Had to change min_change to smaller value
+# Green and grey cluster converge to same value, so we only find 3 clusters.
+
+set.seed(1234567890)
+max_it <- 500 # max number of EM iterations (increased from 100)
+min_change <- 0.01 # min change in log lik between two consecutive iterations
+n=1000 # number of training points
+D=10 # number of dimensions
+x <- matrix(nrow=n, ncol=D) # training data
+
+true_pi <- vector(length = 3) # true mixing coefficients
+true_mu <- matrix(nrow=3, ncol=D) # true conditional distributions
+true_pi=c(1/3, 1/3, 1/3)
+true_mu[1,]=c(0.5,0.6,0.4,0.7,0.3,0.8,0.2,0.9,0.1,1)
+true_mu[2,]=c(0.5,0.4,0.6,0.3,0.7,0.2,0.8,0.1,0.9,0)
+true_mu[3,]=c(0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5)
+plot(true_mu[1,], type="o", col="blue", ylim=c(0,1))
+points(true_mu[2,], type="o", col="red")
+points(true_mu[3,], type="o", col="green")
+# Producing the training data
+for(i in 1:n) {
+  m <- sample(1:3,1,prob=true_pi)
+  for(d in 1:D) {
+    x[i,d] <- rbinom(1,1,true_mu[m,d])
+  }
+}
+
+M=4 # number of clusters
+w <- matrix(nrow=n, ncol=M) # weights
+pi <- vector(length = M) # mixing coefficients
+mu <- matrix(nrow=M, ncol=D) # conditional distributions
+llik <- vector(length = max_it) # log likelihood of the EM iterations
+
+
+# Random initialization of the parameters
+pi <- runif(M,0.49,0.51)
+pi <- pi / sum(pi)
+for(m in 1:M) {
+  mu[m,] <- runif(D,0.49,0.51)
+}
+for(it in 1:max_it) {
+  plot(mu[1,], type="o", col="blue", ylim=c(0,1))
+  points(mu[2,], type="o", col="red")
+  points(mu[3,], type="o", col="darkgreen")
+  points(mu[4,], type="o", col="darkgoldenrod1")
+  Sys.sleep(0.5) # Decreased from 0.5
+  # E-step: Computation of the weights
+  # Calculates the weights for each observation
+  for(iter in 1:1000){
+    x_d <- x[iter, ]
+    
+    # Bernolli for (x1 given mu_1), (x1 given mu_2), (x1 given mu_3), (x1 given mu_4)
+    bern_mu1 <- prod(mu[1,]^x_d * (1 - mu[1, ])^(1-x_d))
+    bern_mu2 <- prod(mu[2,]^x_d * (1 - mu[2, ])^(1-x_d))
+    bern_mu3 <- prod(mu[3,]^x_d * (1 - mu[3, ])^(1-x_d))
+    bern_mu4 <- prod(mu[4,]^x_d * (1 - mu[3, ])^(1-x_d))
+    
+    # Probabilities that the observation belongs to each distribution
+    prob_1 <- pi[1] * bern_mu1 / sum(pi[1]*bern_mu1 + pi[2]*bern_mu2 + pi[3]*bern_mu3 + pi[4]*bern_mu4)
+    prob_2 <- pi[2] * bern_mu2 / sum(pi[1]*bern_mu1 + pi[2]*bern_mu2 + pi[3]*bern_mu3 + pi[4]*bern_mu4)
+    prob_3 <- pi[3] * bern_mu3 / sum(pi[1]*bern_mu1 + pi[2]*bern_mu2 + pi[3]*bern_mu3 + pi[4]*bern_mu4)
+    prob_4 <- pi[4] * bern_mu4 / sum(pi[1]*bern_mu1 + pi[2]*bern_mu2 + pi[3]*bern_mu3 + pi[4]*bern_mu4)
+    # Probability that observation 1 comes from each distribution
+    w[iter, ] <- c(prob_1, prob_2, prob_3, prob_4) 
+  }
+  #Log likelihood computation.
+  # Added 1e-16 to prevent log(0).
+  llik[it] <- sum(log(w+1e-16))
+  cat("iteration: ", it, "log likelihood: ", llik[it], "\n")
+  flush.console()
+  # Stop if the lok likelihood has not changed significantly
+  if (it > 1){
+    if(abs(llik[it] - llik[it-1]) < abs(min_change)){
+      break
+    }
+  }
+  # M-step: ML parameter estimation from the data and weights
+  # Calculate new pi
+  pi <- 1/1000 * colSums(w)
+  # Calculates new mu
+  mu[1,] <- 1/sum(w[,1]) * colSums(w[,1] * x)
+  mu[2,] <- 1/sum(w[,2]) * colSums(w[,2] * x)
+  mu[3,] <- 1/sum(w[,3]) * colSums(w[,3] * x)
+  mu[4,] <- 1/sum(w[,4]) * colSums(w[,4] * x)
+}
+pi
+mu
+plot(llik[1:it], type="o")
